aeb4ce4d7ae6ae2a4bf090f83d2581fafa02c071
unknown <mcvilleanrapsody@gmail.com>
2020-07-22 02:17:29 +0800

    modify config

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,12 @@
 
     $timezone = date_default_timezone_set("Asia/Hong_Kong");
 
-    $con = mysqli_connect(DATABASE, ROOT_USER, ROOT_PASS, DB_NAME);
+    $database = getenv('DATABASE');
+    $rootUser = getenv('ROOT_USER');
+    $rootPass = getenv('ROOT_PASS');
+    $dbName = getenv('DB_NAME');
+
+    $con = mysqli_connect($database, $rootUser, $rootPass, $dbName);
 
     if(mysqli_connect_errno()){
         echo "Failed to connect: ". mysqli_connect_errno();
